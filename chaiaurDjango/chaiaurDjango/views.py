e754dc500f3ea02593b0c6ccff2196baee09da78
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2025-12-12 22:14:35 +0530

    new app

--- chaiaurDjango/chaiaurDjango/views.py
+++ chaiaurDjango/chaiaurDjango/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home(request):
-    return HttpResponse("Hello World!! You are at home page")
+    # return HttpResponse("Hello World!! You are at home page")
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("Hello World!! You are at about page")
