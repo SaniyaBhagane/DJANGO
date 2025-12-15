3440f5f1fca7b2e015dfdbacde06b91d9e9f913f
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2025-12-15 23:29:55 +0530

    models

--- chaiaurDjango/chai/models.py
+++ chaiaurDjango/chai/models.py
@@ -15,4 +15,4 @@ class ChaiVariety(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='chai_varieties/')
     date_added = models.DateTimeField(default=timezone.now)
-    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES)
+    type = models.CharField(max_length=20, choices=CHAI_TYPE_CHOICES)
